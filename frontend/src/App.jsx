0cc1fa65dda95ea4fa43972a7d4cf46a790a91e7
kirubel4 <kirubelayele2127@gmail.com>
2025-06-06 17:47:26 +0300

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import Nav from "./componets/Nav";
+function App() {
+  return (
+    <Nav />
+  )
+}
+
+export default App;
---
+++ frontend/src/componets/Nav.jsx
@@ -0,0 +1,10 @@
+function Nav(laskjdf){
+    return (
+        <div className="flex bg-black">
+            <img src="" alt="log"/>
+
+            
+        </div>
+    )
+}
+export default Nav;
